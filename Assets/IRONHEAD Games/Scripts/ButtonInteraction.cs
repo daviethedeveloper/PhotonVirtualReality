f1756815c1d6e912600b14a3a4fc6879da8bec6c
Davie <107007169+daviethedeveloper@users.noreply.github.com>
2023-02-18 23:51:26 -0700

    [XR Device Simulator]
    * Used XR Device simulator to try out the button press primary button.
    * Able to Console a message to confirm that the button is being pressed
    * Using the Onclick event listener on the button

---
+++ Assets/IRONHEAD Games/Scripts/ButtonInteraction.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+namespace IRONHEAD_Games.Scripts
+{
+    public class ButtonInteraction : MonoBehaviour
+    {
+
+        public void OnButtonPress()
+        {
+            Debug.Log("Button has been pressed");
+        }
+    }
+}
