a2881d1993a72cb8a282cec2a02caa0a75a13a27
Davie <107007169+daviethedeveloper@users.noreply.github.com>
2023-02-21 23:36:38 -0700

    [Custom Room Map Selection]
    * Created Custom Property based on map type selection
    * Used custom property lobby to implement hashtable (keys, values)
    * Next join rooms with specific key values - load scene

--- Assets/IRONHEAD Games/Scripts/RoomManager.cs
+++ Assets/IRONHEAD Games/Scripts/RoomManager.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using ExitGames.Client.Photon;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.Networking;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class RoomManager : MonoBehaviourPunCallbacks
 {
@@ -32,6 +35,15 @@ public class RoomManager : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         Debug.Log("The Local Player player: " + PhotonNetwork.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name + " Player Count: " + PhotonNetwork.CurrentRoom.PlayerCount);
+
+        object myMap;
+        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey(MultiPlayerVRConstants.MAP_TYPE_KEY))
+        {
+            if (PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue(MultiPlayerVRConstants.MAP_TYPE_KEY,out myMap))
+            {
+                Debug.Log("Joined the room with map: " + (string)myMap);
+            }
+        }
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
@@ -48,8 +60,22 @@ public class RoomManager : MonoBehaviourPunCallbacks
         string randomRoomName = "Room_" + Random.Range(0, 10000);
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 20;
+
+        string[] roomPropsInLobby = { MultiPlayerVRConstants.MAP_TYPE_KEY };
+        /*
+         * There are actually two different maps
+         * 1. The school map = "school"
+         * 2. The outdoor map  = "outdoor"
+         */
+
+        ExitGames.Client.Photon.Hashtable customRoomProperties = new ExitGames.Client.Photon.Hashtable() { {MultiPlayerVRConstants.MAP_TYPE_KEY, "school"}};
+        
+        roomOptions.CustomRoomPropertiesForLobby = roomPropsInLobby;
+        roomOptions.CustomRoomProperties = customRoomProperties;
+        
         
         PhotonNetwork.CreateRoom(randomRoomName, roomOptions);
+        
     }
 
     #endregion
