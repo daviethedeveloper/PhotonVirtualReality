a7034b77b37b55fba9327eb3723fd5ec08e48e36
Davie <107007169+daviethedeveloper@users.noreply.github.com>
2023-02-25 18:26:00 -0700

    [Ownership Transfer]
    * Worked on ownership transfer using Photon 2
    * Call ed the RequestOwnership method from the PhotonView of the grabbed object
    * Use call back methods to transfer ownership
    * Weird bug with gravity

--- Assets/IRONHEAD Games/Scripts/NetworkGrabbing.cs
+++ Assets/IRONHEAD Games/Scripts/NetworkGrabbing.cs
@@ -1,10 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class NetworkGrabbing : MonoBehaviour
+public class NetworkGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbacks
 {
-    
+    private PhotonView m_photonView;
+
+    private void Awake()
+    {
+        m_photonView = GetComponent<PhotonView>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +29,39 @@ public class NetworkGrabbing : MonoBehaviour
     public void OnSelectEntered()
     {
         Debug.Log("Grabbed");
+        TransferOwnership();
+    }
+
+    private void TransferOwnership()
+    {
+        m_photonView.RequestOwnership();
     }
 
     public void OnSelectExited()
     {
         Debug.Log("Released");
     }
+
+
+    #region IPun Callbacks
+
+
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+    {
+        Debug.Log("Ownership requested for: " + targetView.name + "from " + requestingPlayer.NickName);
+        m_photonView.TransferOwnership(requestingPlayer);
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        Debug.Log("OnOwnership Transferred to " + targetView.name + "from " + previousOwner.NickName);
+    }
+
+    public void OnOwnershipTransferFailed(PhotonView targetView, Player senderOfFailedRequest)
+    {
+        
+    }
+
+    #endregion
+
 }
